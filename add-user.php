9a79d8aac5fe86218c6c4eb3f6e318f236e16de4
trinity-dennis <92657363+trinity-dennis@users.noreply.github.com>
2023-11-27 02:31:07 -0600

    reverted changes

--- add-user.php
+++ add-user.php
@@ -1,4 +1,5 @@
 <?php
+
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "mysqlpassword";
@@ -6,35 +7,24 @@ $db_name = "main_database";
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
-if (mysqli_connect_errno()) {
-    echo 'Connection to database failed:' . mysqli_connect_error();
+if (mysqli_connect_errno())
+{
+    echo 'Connection to database failed:'.mysqli_connect_error();
     exit();
 }
 
-// Process the form submission
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = mysqli_real_escape_string($conn, $_POST["username"]);
-    $password = mysqli_real_escape_string($conn, $_POST["password"]);
+echo "database connection success<br>";
 
-    // Hash the password (for security)
-    $hashed_password = password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
+$admin = false;
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-    // Set admin to false by default
-    $admin = false;
+$sql = "INSERT INTO users (admin, username, password) VALUES ('$admin', '$username', '$password')";
 
-    // Use prepared statements to prevent SQL injection
-    $stmt = $conn->prepare("INSERT INTO user_table (admin, username, password) VALUES (?, ?, ?)");
-    $stmt->bind_param("sss", $admin, $username, $hashed_password);
-
-    if ($stmt->execute()) {
-        echo "Account created successfully";
-    } else {
-        echo "Error creating account";
-        // Log the error instead of exposing details
-        error_log($stmt->error);
-    }
-
-    $stmt->close();
+if ($conn->query($sql) === TRUE) {
+    echo "Data inserted successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
--- pull-users.js
+++ pull-users.js
@@ -1,60 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Function to fetch user data from the server
-    function pullUsers() {
-      fetch("pull-users.php", {
-        method: "GET",
-      })
-        .then(function (response) {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.json(); // Assuming your server sends JSON data
-        })
-        .then(function (data) {
-          // Handle the user data received from the server
-          console.log(data);
-          // You can update the DOM or perform other actions with the data
-        })
-        .catch(function (error) {
-          // Handle errors (if any)
-          console.error("Error:", error);
-        });
-    }
-  
-    // Call the pullUsers function when the page loads
-    pullUsers();
-  
-    // Function to create a new account
-    window.createAccount = function () {
-      var username = document.getElementById("username").value;
-      var password = document.getElementById("password").value;
-  
-      // Make an AJAX request using the Fetch API
-      fetch("add-user.php", {
-        method: "POST", // Change to POST method
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: username,
-          password: password,
-        }),
-      })
-        .then(function (response) {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.text();
-        })
-        .then(function (data) {
-          // Handle the response from the server (if needed)
-          console.log(data);
-          // Refresh user data after creating a new account
-          pullUsers();
-        })
-        .catch(function (error) {
-          // Handle errors (if any)
-          console.error("Error:", error);
-        });
-    };
-  });
+document.addEventListener('DOMContentLoaded', function () {
+    const userForm = document.getElementById('registerForm');
+
+    userForm.addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevent the default form submission
+
+        const formData = new FormData(userForm);
+
+        // Call the function to send data to the server
+        sendUserDataToDB(formData);
+    });
+});
+
+function sendUserDataToDB(formData) {
+    const username = formData.get('username');
+    const password = formData.get('password');
+
+    // Note: encodeURIComponent is used to properly encode the values in the URL
+    const url = `./add-user.php?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`;
+
+    fetch(url)
+    .then(response => response.text())
+    .then(data => {
+        console.log(data); // Log the response from the PHP script
+        // Add any additional client-side logic here
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        // Handle errors
+    });
+}
