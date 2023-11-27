ebaa5addb2d2ddfa5e8d787d0ed7b1372d4af02b
trinity-dennis <92657363+trinity-dennis@users.noreply.github.com>
2023-11-27 00:11:26 -0600

    added the onlick

---
+++ pull-users.js
@@ -0,0 +1,21 @@
+function sendUserDataToDB() {
+    const username = document.getElementsByName('username')[0].value;
+    const password = document.getElementsByName('password')[0].value;
+
+    fetch('insertData.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`,
+    })
+    .then(response => response.text())
+    .then(data => {
+        console.log(data); // Log the response from the PHP script
+        // Add any additional client-side logic here
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        // Handle errors
+    });
+}
