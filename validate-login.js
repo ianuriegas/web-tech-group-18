8749bc1688c59a248636bfe502f0f7b405c7b1f8
kbi508 <99996743+kbi508@users.noreply.github.com>
2023-11-28 00:39:17 -0600

    changed data recived from type text to json

--- validate-login.js
+++ validate-login.js
@@ -30,7 +30,7 @@ function checkUserCredentials(formData, userForm, failMessage) {
         },
         body: new URLSearchParams(formData).toString(),
     })
-    .then(response => response.text())
+    .then(response => response.json())
     .then(data => {
         console.log('Received data:', data);
 
