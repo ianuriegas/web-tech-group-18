5bfb8978c556115c20a8c0ee1d90145e612f0c38
trinity-dennis <92657363+trinity-dennis@users.noreply.github.com>
2023-11-27 20:42:01 -0600

    added in js and php for login validation

---
+++ user-login.php
@@ -0,0 +1,48 @@
+<?php
+
+error_reporting(E_ALL);
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "mysqlpassword";
+$db_name = "main_database";
+
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+if (mysqli_connect_errno()) {
+    echo 'Connection to database failed: ' . mysqli_connect_error();
+    exit();
+}
+
+echo "Database connection success<br>";
+
+// Start a transaction
+$conn->begin_transaction();
+
+try {
+    
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    
+    // Execute the query
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // Commit the transaction if there is a match
+        $conn->commit();
+        echo "Login successful";
+    } else {
+        // Rollback the transaction if there is an error
+        $conn->rollback();
+        echo "Invalid username or password";
+    }
+} catch (Exception $e) {
+    // Handle exceptions and roll back the transaction
+    $conn->rollback();
+    echo "Exception: " . $e->getMessage();
+}
+
+// Close the database connection
+$conn->close();
+?>
--- validate-login.js
+++ validate-login.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const userForm = document.getElementById('registerForm');
-    const successMessage = document.getElementById('successMessage');
+    const userForm = document.getElementById('loginForm');
+
     userForm.addEventListener('submit', function (event) {
         event.preventDefault(); // Prevent the default form submission
         console.log('Form submitted'); // Add this line for debugging
@@ -8,19 +8,19 @@ document.addEventListener('DOMContentLoaded', function () {
         const formData = new FormData(userForm);
 
         // Call the function to send data to the server
-        sendUserDataToDB(formData, userForm, successMessage);
+        checkUserCredentials(formData, userForm);
 
     });
 });
 
-function sendUserDataToDB(formData, userForm, successMessage) {
+function checkUserCredentials(formData, userForm, failMessage) {
     const username = formData.get('username');
     const password = formData.get('password');
     console.log('Username:', username); // Add this line for debugging
     console.log('Password:', password);
 
     // Note: encodeURIComponent is used to properly encode the values in the URL
-    const url = './add-user.php';
+    const url = './user-login.php';
 
     fetch(url, {
         method: 'POST',
@@ -32,12 +32,13 @@ function sendUserDataToDB(formData, userForm, successMessage) {
     .then(response => response.text())
     .then(data => {
         console.log(data);
+            // Display success message
             console.log('Success!');
-            
-            successMessage.style.display = 'block';
+            if(data == "Invalid username or password") {
+                failMessage.style.display = 'block';
+            }
             // Clear the form
             userForm.reset();
-        //}
     })
     .catch(error => {
         console.error('Error:', error);
