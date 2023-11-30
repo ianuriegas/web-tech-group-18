09e7336b25f5d173bed2aa681ced4680190c1e86
trinity-dennis <92657363+trinity-dennis@users.noreply.github.com>
2023-11-29 20:57:37 -0600

    added in js and php to display users table on admin dashboard - still needs testing on instance

---
+++ fill-users-table.js
@@ -0,0 +1,46 @@
+
+// Function to remove a user
+function removeUser(userId) {
+    // Implement the logic to remove the user (send a request to the server)
+    console.log(`Remove user with ID ${userId}`);
+    // Send an AJAX request to the server to remove the user
+    // ...
+}
+
+// Function to make a user an admin
+function makeAdmin(userId) {
+    // Implement the logic to make the user an admin (send a request to the server)
+    console.log(`Make user with ID ${userId} an admin`);
+    // Send an AJAX request to the server to make the user an admin
+    // ...
+}
+
+// Fetch users from the server and render the table
+async function fetchAndRenderUsers() {
+    const response = await fetch('get_users.php');
+    const users = await response.json();
+    renderUsers(users);
+}
+
+// Function to render users in the table
+function renderUsers(users) {
+    const tableBody = document.getElementById('userTableBody');
+    tableBody.innerHTML = '';
+
+    users.forEach(user => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${user.id}</td>
+            <td>${user.username}</td>
+            <td>${user.admin ? 'Yes' : 'No'}</td>
+            <td>
+                <button onclick="removeUser(${user.id})">Remove</button>
+                <button onclick="makeAdmin(${user.id})">Make Admin</button>
+            </td>
+        `;
+        tableBody.appendChild(row);
+    });
+}
+
+// Initial rendering
+fetchAndRenderUsers();
---
+++ get-users.php
@@ -0,0 +1,35 @@
+<?php
+// database connection
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "mysqlpassword";
+$db_name = "main_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo 'Connection to database failed: ' . mysqli_connect_error();
+    exit();
+}
+
+// fetch users from the database
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+$users = array();
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $users[] = $row;
+    }
+}
+
+// Close the connection
+$conn->close();
+
+// Send JSON response
+header('Content-Type: application/json');
+echo json_encode($users);
+?>
