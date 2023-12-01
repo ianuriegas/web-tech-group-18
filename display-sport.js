8a092a100350710adec2ae01a0df1d1346c78a7c
trinity-dennis <92657363+trinity-dennis@users.noreply.github.com>
2023-12-01 01:03:08 -0600

    added style to username

--- display-sport.js
+++ display-sport.js
@@ -18,6 +18,9 @@ async function fetchAndRenderPosts() {
             row.innerHTML = `
                 <td>
                     <div>
+                      <p style="font-size: x-large; font-family: 'FontAwesome';"><b>${post.username}</b></p>
+                    </div>
+                    <div>
                       ${post.image ? `<img src="./assets/images/${post.image}" alt="Image" style="max-width: 100%; height: auto;">` : ''}
                     </div>
                     <div>
