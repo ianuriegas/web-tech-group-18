4dbfbb5a746e35ab5ae451d0bcdd00d08b6d9b01
kbi508 <99996743+kbi508@users.noreply.github.com>
2023-11-30 23:19:30 -0600

    removed error statement

--- edit-account.js
+++ edit-account.js
@@ -73,10 +73,10 @@ async function updatePassword(users) {
         // Get the new password from the input field
         const newPassword = document.getElementById('newPasswordInput').value;
 
-        if (!users || users.length === 0) {
-            console.error('No users found.');
-            return;
-        }
+        // if (!users || users.length === 0) {
+        //     console.error('No users found.');
+        //     return;
+        // }
 
         // Find the user in the array based on the current username
         const user = users.find(user => user.username === currentUsername);
