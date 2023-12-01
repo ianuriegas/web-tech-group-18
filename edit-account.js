190e026bb823f457653fa3d776f9462282f6edfe
kbi508 <99996743+kbi508@users.noreply.github.com>
2023-11-30 22:30:45 -0600

    made changes to update password

--- edit-account.js
+++ edit-account.js
@@ -10,6 +10,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
         updateUsername(users); // Call your updateUsername function
     });
+
+    const passwordForm = document.getElementById('passwordForm')
+    passwordForm.addEventListener('submit', async function (event) {
+        event.preventDefault(); // Prevent the default form submission behavior
+
+        // Fetch users
+        const response = await fetch('fetch-users.php');
+        const users = await response.json();
+
+        updatePassword(users); // Call your updateUsername function
+    });
 });
 
 async function updateUsername(users) {
@@ -44,6 +55,50 @@ async function updateUsername(users) {
             document.cookie = `username=${newUsername}; path=/`;
             // Close the modal after updating
             closeModal('usernameModal');
+        } else {
+            // Handle error
+            console.error(data.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+
+async function updatePassword(users) {
+    try {
+        // Get the current username from the cookie
+        const currentUsername = getCookie('username');
+
+        // Get the new password from the input field
+        const newPassword = document.getElementById('newPasswordInput').value;
+
+        // Find the user in the array based on the current username
+        const user = users.find(user => user.username === currentUsername);
+
+        if (!user) {
+            console.error('User not found.');
+            return;
+        }
+
+        const response = await fetch('update-password.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ currentUsername, newPassword }),
+        });
+
+        const data = await response.json();
+
+        if (data.status === 'success') {
+            // Update the displayed password
+            document.getElementById('password-info').innerHTML = '********';
+
+            document.cookie = `password=${newPassword}; path=/`;
+
+            // Close the modal after updating
+            closeModal('passwordModal');
         } else {
             // Handle error
             console.error(data.message);
